882025e865b82e1519126fc8a5cc4fe6d9657152
mainuromayer <mainuromayer@gmail.com>
2023-05-07 13:19:53 +0600

    PHP_HTML_Webpage - miligram css and normalize css integration

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <style>
+        body{
+            margin-top: 30px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h1>Welcome</h1>
+                <h2><?php echo "Hello World"; ?></h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt maiores dicta assumenda quos, eveniet dignissimos!</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
